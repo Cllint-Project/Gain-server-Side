71a097541e4056387f2d5390bef5ed38006a4770
shohan15020 <mdshohansajjad@gmail.com>
2024-11-26 02:12:13 +0600

    added home route

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 // app.use('/api/mining', miningRoutes);
 
+app.get('/', async(req,res)=>{
+  res.send('Gain is connected')
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
