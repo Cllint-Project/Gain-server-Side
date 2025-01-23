c2428b400adca5f0f76f2ed3a74fd435865b1836
Sajjadhosenshohan <mdshohansajjad@gmail.com>
2025-01-23 23:03:21 +0600

    fixed corse issue

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const lotteryRoutes = require('./routes/lotteryRoutes');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const lotteryRoutes = require("./routes/lotteryRoutes");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://gain-invest.netlify.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || '')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGODB_URI || "")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/lottery', lotteryRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/lottery", lotteryRoutes);
 
-app.get('/', async(req,res)=>{
-  res.send('Gain is connected')
-})
+app.get("/", async (req, res) => {
+  res.send("Gain is connected");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
